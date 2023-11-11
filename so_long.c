3956e8995efb2845d20c270845eab5e19a157bf7
Juan Bautista Delorme Pinedo <jdelorme@c1r10s1.42madrid.com>
2023-11-11 22:48:04 +0100

    Keyhook add

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: jdelorme <jdelorme@student.42madrid>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 18:16:48 by jdelorme          #+#    #+#             */
-/*   Updated: 2023/11/09 18:24:45 by jdelorme         ###   ########.fr       */
+/*   Updated: 2023/11/11 21:01:48 by jdelorme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,64 @@
 #include <unistd.h>
 #include <stdio.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+# define KEY_ESC		53
+# define KEY_W			13
+# define KEY_A			0
+# define KEY_S			1
+# define KEY_D			2
+# define ARROW_UP		126
+# define ARROW_DOWN		125
+# define ARROW_LEFT		123
+# define ARROW_RIGHT	124
 
-int	ft_deal_key(int key, void *param)
-{
-	int i;
-
-
-	printf("El jugador ha realizado %i movimientos. \n", i);
-		
-	return 0;
-}
-
-int	main(void)
+typedef struct s_vars
 {
 	void	*mlx;
 	void	*mlx_win;
+}				t_vars;
 
-	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1080, 1080, "so_long");
-	mlx_pixel_put(mlx, mlx_win, 250, 250, 0xFFFFFF);
-	mlx_key_hook(mlx_win, ft_deal_key, (void *)0);
-	mlx_loop(mlx);
+
+/*Funcion presionar teclas*/
+int	ft_deal_key(int key, t_vars vars)
+{
+	int steps;
+
+	if (key == KEY_A)
+		printf("Ha presionado A \n");
+	if (key == KEY_W)
+		printf("Ha presionado W \n");
+	if (key == KEY_S)
+		printf("Ha presionado S \n");
+	if (key == KEY_D)
+		printf("Ha presionado D \n");
+	if (key == ARROW_RIGHT)
+		printf("Ha presionado DERECHA \n");
+	if (key == ARROW_LEFT)
+		printf("Ha presionado IZQUIERDA \n");
+	if (key == ARROW_DOWN)
+		printf("Ha presionado ABAJO \n");
+	if (key == ARROW_UP)
+		printf("Ha presionado ARRIBA \n");
+	if (key == KEY_ESC)
+		mlx_destroy_window(vars.mlx, vars.mlx_win);
+	return 0;
+}
+/*Funcion para contar los pasos*/
+void	ft_steps(t_vars vars)
+{
+	
+}
+/*Funcion de lectura del mapa*/
+/*Funcion comprobacion del mapa*/
+ 
+int	main(void)
+{
+	t_vars	vars;
+
+	vars.mlx = mlx_init();
+	vars.mlx_win = mlx_new_window(vars.mlx, 1080, 1080, "so_long");
+	mlx_key_hook(vars.mlx_win, ft_deal_key, &vars);
+	//mlx_loop_hook(vars.mlx, &ft_steps, &vars);
+	mlx_loop(vars.mlx);
 	return 0;
 }
