88524113caf9047bcb939503228b4dba007a525d
Juan Bautista Delorme Pinedo <jdelorme@c1r8s2.42madrid.com>
2023-11-14 19:31:59 +0100

    new map checks added

--- so_long.c
+++ so_long.c
@@ -6,7 +6,7 @@
 /*   By: jdelorme <jdelorme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 18:16:48 by jdelorme          #+#    #+#             */
-/*   Updated: 2023/11/13 19:48:23 by jdelorme         ###   ########.fr       */
+/*   Updated: 2023/11/14 19:31:07 by jdelorme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,26 +46,61 @@ typedef struct s_game
 {
 	char	*game;
 	char 	*map_line;
-	int		map_long;
-	int		map_large;
+	char	**map_copy;
+	int		map_x;
+	int		map_y;
 	int		collectables;
 	int		walls;
 	int		spaces;
 	int		exit;
 }				t_game;
 
-typedef struct s_player
-{
-	int		x;
-	int		y;
-}				t_player;
+/*
 
+					! ZONA GUARRA !
+
+*/
+char	*long_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	if (!s1 && !s2)
+		return (NULL);
+	if (!s1 && s2)
+		return (ft_strdup(s2));
+	i = 0;
+	j = 0;
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (free(s1), NULL);
+	while (s1 != NULL && s1[i] != '\0')
+	{
+		if (str[i] == '\n')
+			i++;
+		str[i] = s1[i];
+		i++;
+	}
+	while (s2 != NULL && s2[j] != '\0')
+	{
+		if (str[i + j] == '\n')
+			j++;
+		str[i + j] = s2[j];
+		j++;
+	}
+	str[i + j] = '\0';
+	return (free(s1), str);
+}
+/*
+
+					! FIN DE ZONA GUARRA !
+
+*/
 void	ft_error(char *error)
 {
-	t_vars	vars;
-	
 	ft_putstr_fd (error, 1);
-	mlx_destroy_window(vars.mlx, vars.mlx_win);
+	exit (1);
 }
 
 void	ft_check_for_elements(char *mapline)
@@ -79,9 +114,6 @@ void	ft_check_for_elements(char *mapline)
 	j = 0;
 	k = 0;
 	e = 0;
-	// ! PRUEBA
-	printf ("%s", mapline);
-	// ! FIN PRUEBA
 	while (mapline[i++])
 	{
 		if (mapline[i] == 'C')
@@ -91,27 +123,75 @@ void	ft_check_for_elements(char *mapline)
 		if (mapline[i] == 'P')
 			e++;
 	}
-	if ((j || k || e) != 1)
+	if (j != 1 || k != 1 || e != 1)
 		ft_error("Error: Invalid Map (Not enough elements)");
 }
+
+// ! Arreglar la condicion del salto de linea
+void	ft_check_for_characters(char *mapline)
+{
+	int	i;
+
+	i = 0;
+	while (mapline[i] != '\0')
+	{
+		if (mapline[i] != 'C' && mapline[i] != 'E' && mapline[i] != 'P' && mapline[i] != '1' && mapline[i] != '0' && mapline[i] != '\n')
+		{
+			ft_error("Error: Invalid map (Invalid symbols)");
+		}
+		i++;
+	}
+}
+// void	ft_check_for_walls(t_game *game)
+// {
+// 	int	i;
+// 	int	j;
+// 	int len_x
+	
+// 	len_x =	strange_strlen()
+// 	i = -1;
+// 	j = -1;
+	
+// 	while (game->map_y[++i])
+// 	{
+// 		if (game->map_y[i] != 1)
+// 			ft_error("Error: Invalid map (Invalid walls)")
+// 		if ()
+// 	}
+	
+// }
+void	ft_check_for_rectangle(t_game *game)
+{
+	int area;
+	int len;
+
+	len = ft_strlen(game->map_line);
+	area = game->map_x * game->map_y;
+	printf("area %i, mapx = %i, mapy = %i, len = %i \n", area, game->map_x, game->map_y, len);
+	if ((len - 3) != area )
+	{
+		ft_error("Error: Invalid map (Not squared)");
+	}
+}
+
 void	ft_check_valid_map(t_game *game)
 {
-	// ! Funcion para comprobar que existan una unica saluda y puntos de partida, y al menos un coleccionable
+	// * Funcion para comprobar que existan una unica saluda y puntos de partida, y al menos un coleccionable
 	ft_check_for_elements(game->map_line);
-	// ! Funcion para comprobar que no existan caracteres distitntos a los validos en el mapa
+	// * Funcion para comprobar que no existan caracteres distitntos a los validos en el mapa
+	ft_check_for_characters(game->map_line);
 	// ! Comprobar que el mapa este enteramente rodeado por muros 
+	//	ft_check_for_walls(&game)
 	// ! Comprobar que exista el menos un camino valido en hasta la salida, y que es camino hacia el coleccionable sea viable
-	// ! Comprobar si e mapa es rectangular
+	// ! Comprobar si e mapa es rectangular (probllema en determinados casos)
+	ft_check_for_rectangle(game);
 }
 
 void	ft_read_map(t_game *game)
 {
 	int		fd;
 	char	*line;
-	// ! llamar a funcion para verificar si es .ber
-	// ! PRUEBA
-	printf ("Breakpoint READMAP");
-	// ! FIN PRUEBA
+	// ! Llamar a funcion para verificar si es .ber
 	fd = open("./Maps/map.ber", O_RDONLY);
 	if (fd < 0)
 	{
@@ -122,18 +202,24 @@ void	ft_read_map(t_game *game)
 	{
 		ft_error("Error: The map is empty");
 	}
+	game->map_x = (ft_strlen(line) - 1);
+	free(line);
+	game->map_line = NULL;
 	while (line)
 	{
+		// ! Problema en el strjoin
+		game->map_line = long_strjoin(game->map_line, line);
 		line = get_next_line(fd);
+		game->map_y++;
+		printf("%s\n", line);
+		// if (game->map_x != ft_strlen(line) - 1)
+		// 	ft_error("TOPOTAMADRE");
 	}
 	close(fd);
-	// ! PRUEBA
-	printf ("%s", line);
-	// ! FIN PRUEBA
 	ft_check_valid_map(game);
 }
 /*Funcion presionar teclas*/
-int	ft_deal_key(int key, t_vars vars)
+int	ft_deal_key(int key, t_vars *vars)
 {
 	int steps;
 
@@ -154,7 +240,11 @@ int	ft_deal_key(int key, t_vars vars)
 	if (key == ARROW_UP)
 		printf("Ha presionado ARRIBA \n");
 	if (key == KEY_ESC)
-		mlx_destroy_window(vars.mlx, vars.mlx_win);
+	{
+		printf("Ha presionado ESC \n");
+		mlx_destroy_window(vars->mlx, vars->mlx_win);
+		exit (1);
+	}
 	return 0;
 }
 
@@ -166,8 +256,8 @@ int	main(void)
 	vars.mlx = mlx_init();
 	vars.mlx_win = mlx_new_window(vars.mlx, 1080, 1080, "so_long");
 	mlx_key_hook(vars.mlx_win, ft_deal_key, &vars);
+	ft_read_map(&game);
 	// ! mlx_loop_hook(vars.mlx, &ft_steps, &vars);
 	mlx_loop(vars.mlx);
-	ft_read_map(&game);
 	return 0;
 }
